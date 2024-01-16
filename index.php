c9e027027739f945eaf5603fea4a331fa14f6a83
NGENIOUS SOLUTIONS <3233354+nilaish@users.noreply.github.com>
2024-01-15 23:52:40 -0500

    Update index.php
    Back to original source code.

--- index.php
+++ index.php
@@ -1,16 +1,2 @@
 <?php
 echo "Hello World!";
-
-// Display an external page using an iframe
-var src = "https://webchat.botframework.com/embed/NMLuisTextQuery-bot?s=wYvQrbSkmWQ.wKg_HdWwrujUThZge5b3mcyHVL0BA57C6iUMlZ8Ptro";
-$.modal('<iframe src="' + src + '" height="450" width="830" style="border:0">', {
-    closeHTML:"",
-    containerCss:{
-        backgroundColor:"#fff",
-        borderColor:"#fff",
-        height:450,
-        padding:0,
-        width:830
-    },
-    overlayClose:true
-});
