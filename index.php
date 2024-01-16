10ebca29ac580c02a7cba0449440e6c4a86709bd
NGENIOUS SOLUTIONS <3233354+nilaish@users.noreply.github.com>
2024-01-16 00:19:35 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,4 @@
 <?php
 echo "Hello World!";
+
+echo "<iframe src='https://webchat.botframework.com/embed/NMLuisTextQuery-bot?s=wYvQrbSkmWQ.wKg_HdWwrujUThZge5b3mcyHVL0BA57C6iUMlZ8Ptro'  style='min-width: 100px; min-height: 500px;'></iframe>";
