1e2e9844c4736dc9652822ba54ded246f588a5b7
NGENIOUS SOLUTIONS <3233354+nilaish@users.noreply.github.com>
2024-01-15 23:31:52 -0500

    Update index.php
    Updating code for iframe.

--- index.php
+++ index.php
@@ -2,5 +2,5 @@
 
 echo "Hello World!";
 
-echo '<iframe src='https://webchat.botframework.com/embed/NMLuisTextQuery-bot?s=wYvQrbSkmWQ.wKg_HdWwrujUThZge5b3mcyHVL0BA57C6iUMlZ8Ptro'  style='min-width: 400px; width: 100%; min-height: 500px;'></iframe>';
+echo "<iframe src='https://webchat.botframework.com/embed/NMLuisTextQuery-bot?s=wYvQrbSkmWQ.wKg_HdWwrujUThZge5b3mcyHVL0BA57C6iUMlZ8Ptro'  style='min-width: 400px; width: 100%; min-height: 500px;'></iframe>";
 ?>
